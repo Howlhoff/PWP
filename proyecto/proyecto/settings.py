1fcea6eb748c002a6df2aa9dddbca78ee57dbdc0
Howlhoff <diego.beltran@sansano.usm.cl>
2021-11-23 13:27:20 -0300

    aaa

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1%t)38m&@2hb(q4bwlowrw%0@bydu10-@#9(=h&&)f^avadoiy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
